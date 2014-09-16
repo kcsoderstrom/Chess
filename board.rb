ce91daf1ebebf598826d31d2996d8bd00d2b4cdb
App Academy Student <student@appacademy.io>
2014-09-16 12:19:49 -0400

    Phase IV complete

--- board.rb
+++ board.rb
@@ -2,6 +2,8 @@ require_relative 'piece' #might not need this wedk
 
 class Board
 
+  COLORS = [:white, :black]
+
   attr_reader :rows
 
   def initialize
@@ -9,25 +11,49 @@ class Board
     #place pieces in their initial positions
   end
 
+  def opposite(color)
+    color == COLORS[0] ? COLORS[1] : color
+  end
+
   def [](pos)
     self.rows[pos[0]][pos[1]]
   end
 
-  def []=(pos, value)     #value is a terrible name change later
-    self.rows[pos[0]][pos[1]] = value
+  def []=(pos, piece)
+    self.rows[pos[0]][pos[1]] = piece
   end
 
-  def in_check?(player)
-    #find king
-    #position not in place of opp-color's potential moves
+  def in_check?(color)
+    king = all_pieces(color).select do |piece|
+      piece.is_a?(King) && piece.color == color
+    end[0]
+
+    all_pieces(opposite(color)).any? { |piece| piece.moves.include?(king.pos) }
+  end
+
+  def check_mate?(color)
+    all_pieces(color).all? { |piece| piece.valid_moves.empty? } &&
+    in_check?(color)  #need to test #valid_moves not written
   end
 
   def move(start, end_pos)
-    raise "No piece at that position." if self[start] == nil
+
+    raise "No piece at that position." if self[start].nil?
     raise "Invalid move." unless self[start].moves.include?(end_pos)
-    raise "Cannot take the king." if self[end_pos].is_a?(King) #make that a class ok?
+    unless self[end_pos].nil?
+      raise "Cannot take the king." if self[end_pos].is_a?(King)
+      if self[end_pos].color == self[start].color
+        raise "Cannot take a piece of your own color"
+      end
+    end
+
     self[start], self[end_pos] = nil, self[start]
+    update_pos(self[end_pos], end_pos)      #this seems stupid
     #might want to implement a .taken for self[end_pos]
+  end
+
+  def update_pos(piece, new_pos)
+    piece.pos = new_pos           #that shouldn't be doable make some privates
   end
 
   def dup
@@ -36,8 +62,11 @@ class Board
     duped
   end
 
-  protected
+  def all_pieces(color)
+    self.rows.flatten.select { |piece| piece.color == color }
+  end
 
+  protected
   attr_writer :rows
 
 end
--- piece.rb
+++ piece.rb
@@ -2,22 +2,41 @@ require_relative 'board'
 
 class Piece
 
-  attr_reader :pos, :board, :color
+  attr_accessor :pos
+
+  attr_reader :board, :color
 
   def initialize(board = Board.new, color = :white, pos = [0, 0])
     @color = color
     @pos = pos
     @board = board
+    @board[pos] = self
   end
 
   def moves
+    [[0,1]]
   end
 
   def move_into_check?(pos)
    test_board = self.board.dup
    test_board[self.pos] = nil
-   test_board[pos] = self.class.new(self.color, pos, test_board)
+   test_board[pos] = self.class.new(test_board, self.color, pos )
    test_board.in_check?(self.color)
+  end
+
+  def valid_moves
+    moves.reject { |move| move_into_check?(move) }.select(&:legal?)
+  end
+
+  def legal?(new_pos)
+    test_board = self.board.dup
+    test_board[self.pos] = self.class.new(test_board, self.color, self.pos)
+    begin
+      test_board.move(self.pos, new_pos)
+    rescue RuntimeError
+      return false
+    end
+    true
   end
 
 end
@@ -30,4 +49,7 @@ end
 
 class SteppingPiece < Piece
 
+end
+
+class King < SteppingPiece
 end
